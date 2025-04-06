2c77138a3bc68b2e1da32856a868696c9c891ed9
Armando-coder-gif <armando.gonzalez@correo.unimet.edu.ve>
2025-04-06 12:52:48 -0600

    accordion

--- src/app/dashboard/accordion/page.tsx
+++ src/app/dashboard/accordion/page.tsx
@@ -1,8 +1,37 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const items = [
+  {
+    id: "item-1",
+    question: "Is it accessible?",
+    answer: "Yes. It adheres to the WAI-ARIA design pattern.",
+  },
+  {
+    id: "item-2",
+    question: "Is otherwise usable?",
+    answer: "No, it is not usable.",
+  },
+  {
+    id: "item-3",
+    question: "Is it keyboard navigable?",
+    answer: "Yes. It supports keyboard navigation.",
+  },
+];
 
 export default function Page() {
   return (
-    <div>
-      <h1> Accordion page</h1>
-    </div>
+    <Accordion type="multiple">
+      {items.map((item) => (
+        <AccordionItem key={item.id} value={item.id}>
+          <AccordionTrigger>{item.question}</AccordionTrigger>
+          <AccordionContent>{item.answer}</AccordionContent>
+        </AccordionItem>
+      ))}
+    </Accordion>
   );
 }
